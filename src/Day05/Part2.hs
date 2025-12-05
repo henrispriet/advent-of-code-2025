a2d3dcdc7fa99b3806e91571f839fa87d1b35031
henrispriet <henri.spriet@gmail.com>
2025-12-05 17:15:28 +0100

    Day05.Part2 i cant anymore
    this is eating too much of my time

--- src/Day05/Part2.hs
+++ src/Day05/Part2.hs
@@ -4,7 +4,7 @@ import Data.Ix
 import Day05.Part1 (testInput)
 import qualified Day05.Part1 (parse)
 
--- import Debug.Trace
+import Debug.Trace
 
 run :: IO String
 run = do
@@ -28,12 +28,41 @@ parse = fst . Day05.Part1.parse
 
 -- Algorithm
 
-rangeIntersection :: (Ord a, Ord b) => (a, b) -> (a, b) -> (a, b)
+rangeIntersection :: Range -> Range -> Range
 rangeIntersection (i1, j1) (i2, j2) = (max i1 i2, min j1 j2)
+
+rangeUnion :: Range -> Range -> MultiRange
+rangeUnion r1 r2
+  | rangeSize (rangeIntersection r1 r2) == 0 = [r1, r2]
+  | otherwise = do
+    let (i1, j1) = r1
+    let (i2, j2) = r2
+    [(min i1 i2, max i1 i2 - 1), (max i1 i2, min j1 j2), (min j1 j2 + 1, max j1 j2)]
+
+rangeSub :: Range -> Range -> Range
+rangeSub r1 r2
+  -- no intersection
+  | rangeSize (rangeIntersection r1 r2) == 0 = r1
+  -- r1 is left of r2
+  | fst (rangeIntersection r1 r2) == fst r2 = (fst r1, fst r2 - 1)
+  -- r1 is right of r2
+  | fst (rangeIntersection r1 r2) == fst r1 = (snd r2 + 1, snd r1)
+  | otherwise = error "bad stuff"
+
+multiRangeUnion :: MultiRange -> Range -> MultiRange
+multiRangeUnion [] r = [r]
+multiRangeUnion (mr:mrs) r
+  | rangeSize r == 0 = (mr:mrs)
+  | otherwise = mr : multiRangeUnion mrs (r `rangeSub` mr)
+
+multiRangeSize :: MultiRange -> Int
+multiRangeSize = sum . map rangeSize
+
+type MultiRange = [Range]
 
 pairs :: [a] -> [(a, a)]
 pairs [] = []
 pairs (x : xs) = map (x,) xs ++ pairs xs
 
 solve :: Problem -> Solution
-solve ranges = sum (map rangeSize ranges) - sum (map (rangeSize . uncurry rangeIntersection) (pairs ranges))
+solve = multiRangeSize . traceShowId . foldl multiRangeUnion []
